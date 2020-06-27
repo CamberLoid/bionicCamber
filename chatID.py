fe7b7c104824ee92469077306c695d312f3d4d91
Billy Huang <camber@poi.science>
2020-06-28 00:03:23 +0800

    chatID minor fix

--- chatID.py
+++ chatID.py
@@ -1,5 +1,5 @@
-# bionicCamber/chatID.py
-# 收到/id时回复
+# bionicCamber/chatID.py | 0.9 pre-Stable
+# 收到/chatid时回复
 # 测试用途.
 
 import pyrogram
@@ -8,7 +8,7 @@ import api
 
 telebot = api.telebot
 
-@telebot.on_message(pyrogram.Filters.command(["chatid", "chatid@"+telebot.get_me.username]))
+@telebot.on_message(pyrogram.Filters.command(["chatid", "chatid@"+telebot.get_me().username]))
 def ChatIDHandler(client:pyrogram.Client, message:pyrogram.Message):
     logging.warn("/Chatid recieved at " + str(message.chat.id))
     replyText = "这里是拱拱的工具拱喵. 现在放送Chat相关信息: \
@@ -16,15 +16,13 @@ def ChatIDHandler(client:pyrogram.Client, message:pyrogram.Message):
     \nChat type= {chatType}\
     \nAdditional Properties= {additional}\
     \n本命令作为Debug用, 这说明了仿生拱拱的网络功能一切正常.\
-    \nFrom bionicCamber/ChannelID 0.9 stable"
+    \nFrom bionicCamber/ChannelID 0.9 pre-stable"
     chatID=message.chat.id
     client.send_message(
         reply_to_message_id=message.message_id,
         text=replyText.format(chatID=chatID, chatType=message.chat.type, additional=None),
         chat_id=message.chat.id)
     pass
-
-@telebot.on_message(pyrogram.Filters.command(["about", "about@atCambot"]))
 
 if(__name__=="__main__"):
     telebot.run()
